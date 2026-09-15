b1e8408e61604a1e68a40122a6cd1cd401deeb29
riarumoda <justinebudiwijaya@gmail.com>
2026-09-15 15:17:28 +0700

    env: wraps toolchains downloads with github token
    Signed-off-by: riarumoda <justinebudiwijaya@gmail.com>

--- scripts/env.sh
+++ scripts/env.sh
@@ -37,7 +37,11 @@ if [[ "$CLANG_STRAT" == "1" ]]; then
             ARCH=arm64 LLVM=1 LLVM_IAS=1 LD=ld.lld
             CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE_COMPAT=arm-linux-gnueabi- CROSS_COMPILE_ARM32=arm-linux-gnueabi-
     )
-    TC_URLS=$(curl -s https://api.github.com/repos/Neutron-Toolchains/clang-build-catalogue/releases/latest | grep "browser_download_url" | head -n 1 | cut -d '"' -f 4)
+    if [[ "$GITHUB_TOKEN" == "" ]]; then
+        TC_URLS=$(curl -s https://api.github.com/repos/Neutron-Toolchains/clang-build-catalogue/releases/latest | grep "browser_download_url" | head -n 1 | cut -d '"' -f 4)
+    else
+        TC_URLS=$(curl -s -H "Authorization: Bearer $GITHUB_TOKEN" https://api.github.com/repos/Neutron-Toolchains/clang-build-catalogue/releases/latest | grep "browser_download_url" | head -n 1 | cut -d '"' -f 4)
+    fi
 elif [[ "$CLANG_STRAT" == "2" ]]; then
     echo "-- Using AOSP Clang + Eva GCC to compile the kernel"
     export CLANG_ROOT="$PWD/clang"
@@ -49,7 +53,11 @@ elif [[ "$CLANG_STRAT" == "2" ]]; then
             CROSS_COMPILE_COMPAT=$GCC32_DIR/bin/arm-eabi-
     )
     TC_URLS_CLANG=("clang|https://gitlab.com/crdroidandroid/android_prebuilts_clang_host_linux-x86_clang-r547379.git")
-    TC_URLS_GCC64=$(curl -s https://api.github.com/repos/mvaisakh/gcc-build/releases/latest | grep "browser_download_url" | cut -d '"' -f 4 | grep -E "eva-gcc-arm.*\.xz")
+    if [[ "$GITHUB_TOKEN" == "" ]]; then
+        TC_URLS_GCC64=$(curl -s https://api.github.com/repos/mvaisakh/gcc-build/releases/latest | grep "browser_download_url" | cut -d '"' -f 4 | grep -E "eva-gcc-arm.*\.xz")
+    else
+        TC_URLS_GCC64=$(curl -s -H "Authorization: Bearer $GITHUB_TOKEN" https://api.github.com/repos/mvaisakh/gcc-build/releases/latest | grep "browser_download_url" | cut -d '"' -f 4 | grep -E "eva-gcc-arm.*\.xz")
+    fi
 elif [[ "$CLANG_STRAT" == "3" ]]; then
     echo "-- Using Playground TC to compile the kernel"
     export CLANG_ROOT="$PWD/clang"
@@ -137,7 +145,11 @@ elif [[ "$CLANG_STRAT" == "2" ]]; then
 elif [[ "$CLANG_STRAT" == "3" ]]; then
     echo "-- Downloading Playground TC..."
     for url in $TC_URLS; do
-      wget -q "$url"
+        if [[ "$GITHUB_TOKEN" == "" ]]; then
+            wget -q "$url"
+        else
+            wget --header="Authorization: Bearer $GITHUB_TOKEN" -q "$url"
+        fi
     done
     echo "-- Extracting Clang..."
     mkdir -p clang
